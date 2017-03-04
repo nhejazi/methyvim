c33177445ac5ebcbcab6217cecac38870fdeb8dc
Nima Hejazi <nhejazi@berkeley.edu>
2017-03-04 13:21:49 -0800

    Create screen_npvi.R

---
+++ R/screen_npvi.R
@@ -0,0 +1,9 @@
+#' Screening procedure using nonparametric variable importance
+#'
+#' @importFrom tmle.npvi PLACEHOLDER
+#'
+#' @export npvi_screen
+#'
+npvi_screen <- function(...) {
+  # placeholder
+}
