99956f712bf769b4bc300f985e88e8f9a6ea4bed
Nima Hejazi <nh@nimahejazi.org>
2017-07-13 18:43:26 -0700

    add more pkg basics

--- R/methadapt.R
+++ R/methadapt.R
@@ -1,10 +1,18 @@
-#' constructor for class methadapt (subclass of SummarizedExperiment)
+#' Constructor for class methyvim
 #'
-#' @return ".methadapt" S4 object (subclass of "SummarizedExperiment") for DNA
-#'         methylation analysis using targeted minimum loss-based estimation.
+#' @return class \code{methyvim} object, sub-classed from SummarizedExperiment.
 #'
-.methadapt <- setClass(
-  "methadapt",
-  contains = "SummarizedExperiment",
-  slots = list(call = "call")
+#' @importFrom methods setClass
+#' @importFrom tibble data_frame
+#' @importClassesFrom SummarizedExperiment SummarizedExperiment
+#'
+#' @export .methyvim
+#' @exportClass methyvim
+#'
+.methyvim <- methods::setClass(
+       Class = "methyvim",
+       slots = list(call = "call",
+                    tmleOut = "data_frame",
+                    topTable = "data_frame"),
+       contains = "SummarizedExperiment"
 )
