8fbf5031ae648303d0849a3f924bdda1c2e62305
Nima Hejazi <nh@nimahejazi.org>
2017-10-11 01:46:22 -0700

    WIP => Active; misc other changes

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(...) {
   packageStartupMessage(paste("methyvim: Nonparametric Differential",
-                              "Methylation Analysis \n with Targeted Estimates",
-                              "of Variable Importance Measures"))
+                              "Methylation Analysis \n using Targeted",
+                              "Estimates of Variable Importance Measures"))
   packageStartupMessage("Version: ",
                         utils::packageDescription("methyvim")$Version)
 }
